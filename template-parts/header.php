b9ceeefb450edab04a76f8e2adf5151b09045590
Alekei <alexander.rasputin@uni-ulm.de>
2018-06-29 00:20:18 +0200

    trying to find error

--- template-parts/header.php
+++ template-parts/header.php
@@ -3,13 +3,6 @@
 		<div class="uk-position-top">
 			<nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
 				<div class="uk-navbar-right">
-					<?php 
-						wp_nav_menu( array(
-							'theme_location' => 'top_menu',
-							'container' => false,
-							'items_wrap' => '<ul class="uk-navbar-nav uk-visible@m" uk-scrollspy-nav="scroll: true" uk-scroll>%3$s</ul>',
-						));
-					?>
 					<a uk-navbar-toggle-icon="" 
 						href="#offcanvas-menu" 
 						uk-toggle="" 
@@ -23,7 +16,10 @@
 		<div class="alekei-hero-content uk-position-center">
 			<div class="uk-flex uk-flex-column uk-flex-middle uk-flex-center">
 				<h1 class="hero-title">
+					<?php echo the_field('hero_welcome_message'); ?>
+					<span> <?php echo the_field('hero_user_name'); ?></span>
 				</h1>
+				<h2 class="hero-subtitle"><?php echo the_field('hero_subtitle'); ?></h2>
 				<div class="page-link-portfolio uk-animation-toggle">
 					<a class="uk-animation-slide-top-small"  href="#portfolio" uk-icon="icon: chevron-down; ratio: 2" uk-scroll></a>
 				</div>
